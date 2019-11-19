e58157b3d749a5c801b8831a7a970115087fbd4c
LeonardoAmorimjob <leonardo.amorim.job@gmail.com>
2019-11-18 21:42:28 -0300

    atividades mobile

---
+++ projetofirebase/www/firebaseAppInit.js
@@ -0,0 +1,8 @@
+//https://firebase.google.com/docs/web/setup?hl=pt-br
+// Initialize Firebase
+
+function onLoadFunction(){
+    firebase.initializeApp(firebaseConfig);
+    showCurrentUser();//update lbUserLoggedIn label
+}
+
---
+++ projetofirebase/www/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+// Initialize Firebase
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBijxovdP5kdGweY-XRtuytdKWRfkCFd_k",
+    authDomain: "aulafirebase-cbad9.firebaseapp.com",
+    databaseURL: "https://aulafirebase-cbad9.firebaseio.com",
+    projectId: "aulafirebase-cbad9",
+    storageBucket: "aulafirebase-cbad9.appspot.com",
+    messagingSenderId: "809427242055",
+    appId: "1:809427242055:web:8cb8203886bd177cfd7562",
+    measurementId: "G-JZF2L6TWHB"
+  };
+ 
+
+
